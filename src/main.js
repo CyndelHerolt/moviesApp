5b4b07e1c190b938d342bc5171bf26b3a6207f4b
CyndelHerolt <herolt.cyndel@gmail.com>
2023-09-29 10:04:10 +0200

    feat [chore]: install Bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 const app = createApp(App)
 
