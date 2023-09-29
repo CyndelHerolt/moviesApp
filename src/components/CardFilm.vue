e91343cd8ecc1c555fc9ab09bcf7a1e45ecfb296
CyndelHerolt <herolt.cyndel@gmail.com>
2023-09-29 10:20:34 +0200

    feat: Card movie

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -3,19 +3,18 @@ const props = defineProps(['film'])
 const movie = props.film
 
 const date = new Date(movie.releaseDate)
-const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+const options = {year: 'numeric', month: 'numeric', day: 'numeric'};
 movie.releaseDate = date.toLocaleDateString('fr-FR', options)
 </script>
 
 <template>
-  <h1>test</h1>
-<h2>{{movie.title}}</h2>
-  <em>{{movie.description}}</em>
+  <h2>{{ movie.title }}</h2>
+  <em>{{ movie.description }}</em>
   <hr>
   <div>
-    <small>{{movie.releaseDate}}</small>
+    <small>{{ movie.releaseDate }}</small>
     <span> | </span>
-    <small>{{movie.duration}} minutes</small>
+    <small>{{ movie.duration }} minutes</small>
   </div>
 </template>
 
