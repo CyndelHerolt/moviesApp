302a96f012fc60f1f92be84a78d99a6556a707fc
CyndelHerolt <herolt.cyndel@gmail.com>
2023-10-16 11:47:31 +0200

    feat [Actors]: page actors

--- src/components/CardActeur.vue
+++ src/components/CardActeur.vue
@@ -4,9 +4,13 @@ const actor = props.acteur
 </script>
 
 <template>
-  <div class="card">
+  <div class="card actor">
     <h3>{{ actor.firstName }} {{ actor.lastName }}</h3>
     <small v-for="nationalite in actor.nationalite">{{nationalite}}</small>
+    <hr>
+    <ul>
+      <li v-for="movie in actor.movies"><routerLink :to="'/fiche-movie/'+movie.id" class="link">{{ movie.title }}</routerLink></li>
+    </ul>
   </div>
 </template>
 
