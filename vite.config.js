8879729355eb9c08273492f668d0a60378076c21
CyndelHerolt <herolt.cyndel@gmail.com>
2024-03-04 14:58:55 +0100

    fix: include img in build output

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // Fonction servant à copier les fichiers du répertoire public/img
+  // dans le répertoire de sortie lors du build
+  // Cette fonction sera exécutée avant le processus de build
+  // Cela garantit que les fichiers sont disponibles lors de la construction
+  optimizeDeps: {
+    include: ['src/assets/posters/**']
   }
 })
