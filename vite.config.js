25c29060c3caf384b77544ca3e32c7c509fa21f9
CyndelHerolt <herolt.cyndel@gmail.com>
2024-03-04 15:16:32 +0100

    fix: include img in build output

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import copy from 'vite-plugin-copy'
+let copy
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    copy({
-      targets: [
-        { src: 'src/assets/posters/*', dest: 'dist/src/assets/posters' }
-      ],
-      hook: 'writeBundle' // exécuté après le processus de build
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  optimizeDeps: {
-    include: ['src/assets/posters/**']
+export default async () => {
+  if (!copy) {
+    copy = (await import('vite-plugin-copy')).default
   }
-})
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      copy({
+        targets: [
+          { src: 'src/assets/posters/*', dest: 'dist/src/assets/posters' }
+        ],
+        hook: 'writeBundle' // exécuté après le processus de build
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    optimizeDeps: {
+      include: ['src/assets/posters/**']
+    }
+  })
+}
