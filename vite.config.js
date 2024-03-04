edd1d1761dfbbbfa78122e643c4a7c73535c770b
CyndelHerolt <herolt.cyndel@gmail.com>
2024-03-04 15:05:55 +0100

    fix: include img in build output

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
+// vite.config.js
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  build: {
+    rollupOptions: {
+      input: {
+        // Spécifiez le fichier HTML principal de votre application
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        // Spécifiez le répertoire de sortie du build
+        dir: 'dist'
+      }
     }
   },
   // Fonction servant à copier les fichiers du répertoire public/img
